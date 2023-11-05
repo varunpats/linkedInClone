34b4fb3382f93beb180dc255648c24afb6738f54
varunpats <varunpatil2502@gmail.com>
2023-11-05 11:14:10 +0530

    S

--- linkedin-clone/src/components/LoginCmp.jsx
+++ linkedin-clone/src/components/LoginCmp.jsx
@@ -27,22 +27,44 @@ export default function LoginCmp() {
     }
 
     return (
-        <div className='login-wrapper'>
-            <img src={LinkedinLogo} className='linkedinLogo' />
-            <div style={{marginLeft: '40%'}}>
-                <h1 className='heading' style={{marginLeft: '-1%'}}>Sign in</h1>
-                <p className='sub-heading' style={{marginLeft: '-1%'}}>Stay updated on your professional world</p>
-                <input className='common-input' style={{marginLeft: '-1%'}} type='email' placeholder='Email or Phone' onChange={(e) => setCred({ ...cred, email: e.target.value })} /><br />
-                <input className='common-input' style={{marginLeft: '-1%'}} type='password' placeholder='Password' onChange={(e) => setCred({ ...cred, password: e.target.value })} /><br />
-                <button onClick={onLogin} className='login-btn'>Sign in</button><br />
+        <div className="login-wrapper">
+            <img src={LinkedinLogo} className="linkedinLogo" />
+
+            <div className="login-wrapper-inner">
+                <h1 className="heading">Sign in</h1>
+                <p className="sub-heading">Stay updated on your professional world</p>
+
+                <div className="auth-inputs">
+                    <input
+                        onChange={(event) =>
+                            setCred({ ...cred, email: event.target.value })
+                        }
+                        type="email"
+                        style={{ marginLeft: '-3%' }}
+                        className="common-input"
+                        placeholder="Email or Phone"
+                    />
+                    <input
+                        onChange={(event) =>
+                            setCred({ ...cred, password: event.target.value })
+                        }
+                        type="password"
+                        style={{ marginLeft: '-3%' }}
+                        className="common-input"
+                        placeholder="Password"
+                    />
+                </div>
+                <button onClick={onLogin} className="login-btn">
+                    Sign in
+                </button>
             </div>
-            <hr className='hr-text' data-content='or' />
+            <hr className="hr-text" data-content="or" />
             <div className='google-btn-container'>
                 <GoogleButton
                     className='google-btn'
                     onClick={googleSignIn}
                 />
-                <p>New to LinkedIn? <span className='join-now' onClick={() => navigate('register')}>Join now</span></p>
+                <p>New to LinkedIn?{" "} <span className='join-now' onClick={() => navigate('/register')}>Join now</span></p>
             </div>
         </div>
     )
--- linkedin-clone/src/components/RegisterCmp.jsx
+++ linkedin-clone/src/components/RegisterCmp.jsx
@@ -26,18 +26,68 @@ export default function RegisterCmp() {
         let response = GoogleSignInAPI();
     }
 
+    // return (
+    //     <div className='login-wrapper'>
+    //         <img src={LinkedinLogo} className='linkedinLogo' />
+    //         <div className='login-wrapper' style={{ marginLeft: '40%' }}>
+    //             <h2 className='heading' style={{marginLeft:'-7%'}}>Make the most of your professional life</h2>
+    //             <label style={{marginLeft:'-7%'}}>Email or phone number</label><br />
+    //             <input className='common-input' style={{width: '405px', marginLeft:'-7%'}} type='email' onChange={(e) => setCred({ ...cred, email: e.target.value })} /><br />
+    //             <label style={{marginLeft:'-7%'}}>Password (6 or more character)</label><br />
+    //             <input className='common-input' style={{width: '405px', marginLeft:'-7%'}} type='password' onChange={(e) => setCred({ ...cred, password: e.target.value })} /><br />
+    //             <button onClick={onRegister} className='login-btn'>Agree & Join</button><br />
+    //         </div>
+    //         <hr className='hr-text' data-content='or' />
+    //         <div className='google-btn-container'>
+    //             <GoogleButton
+    //                 className='google-btn'
+    //                 onClick={googleSignIn}
+    //             />
+    //             <p>Already on LinkedIn? <span className='join-now' onClick={() => navigate('/')}>Sign in</span></p>
+    //         </div>
+    //     </div>
+    // )
+
     return (
-        <div className='login-wrapper'>
-            <img src={LinkedinLogo} className='linkedinLogo' />
-            <div className='login-wrapper' style={{ marginLeft: '40%' }}>
-                <h2 className='heading' style={{marginLeft:'-7%'}}>Make the most of your professional life</h2>
-                <label style={{marginLeft:'-7%'}}>Email or phone number</label><br />
-                <input className='common-input' style={{width: '405px', marginLeft:'-7%'}} type='email' onChange={(e) => setCred({ ...cred, email: e.target.value })} /><br />
-                <label style={{marginLeft:'-7%'}}>Password (6 or more character)</label><br />
-                <input className='common-input' style={{width: '405px', marginLeft:'-7%'}} type='password' onChange={(e) => setCred({ ...cred, password: e.target.value })} /><br />
-                <button onClick={onRegister} className='login-btn'>Agree & Join</button><br />
+        <div className="login-wrapper">
+            <img src={LinkedinLogo} className="linkedinLogo" />
+
+            <div className="login-wrapper-inner">
+                <h1 className="heading">Make the most of your professional life</h1>
+                <div className="auth-inputs">
+                    <label style={{ marginLeft: '-20%' }}>Name</label>
+                    <input
+                        style={{ width: '405px', marginLeft: '-20%' }}
+                        onChange={(event) =>
+                            setCred({ ...cred, name: event.target.value })
+                        }
+                        type="text"
+                        className="common-input"
+                    />
+                    <label style={{ marginLeft: '-20%' }}>Email or phone number</label>
+                    <input
+                        style={{ width: '405px', marginLeft: '-20%' }}
+                        onChange={(event) =>
+                            setCred({ ...cred, email: event.target.value })
+                        }
+                        type="email"
+                        className="common-input"
+                    />
+                    <label style={{ marginLeft: '-20%' }}>Password (6 or more characters)</label>
+                    <input
+                        style={{ width: '405px', marginLeft: '-20%' }}
+                        onChange={(event) =>
+                            setCred({ ...cred, password: event.target.value })
+                        }
+                        type="password"
+                        className="common-input"
+                    />
+                </div>
+                <button onClick={onRegister} className="login-btn">
+                    Agree & Join
+                </button>
             </div>
-            <hr className='hr-text' data-content='or' />
+            <hr class="hr-text" data-content="or" />
             <div className='google-btn-container'>
                 <GoogleButton
                     className='google-btn'
